876dfdf616ebb74f3f4567cc4432e4e3ff962c98
Abhi-AIX <aibhi.dev@gmail.com>
2025-09-26 07:48:52 -0500

    Add initial solutions for Greedy and Recursion problems

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("====================Greedy=====================");
+
+Console.WriteLine("====================Divide And Conquer=====================");
+Console.WriteLine("Polynamial Multiplication:");
+PolynamialMultiplication pm = new PolynamialMultiplication();
+int[] A = { 2, 4 };
+int[] B = { 3, 1 };
+int[] resultArray = pm.Multiply(A, B);
+Console.WriteLine($"{string.Join(",", resultArray)}");
+
+Console.WriteLine();
+Console.WriteLine("Recursion PrintOne:");
+Fundamental.Print();
